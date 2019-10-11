5bdb96e82c622064a60775cb249b92ae78eb0691
Roel <roelverschuijten@gmail.com>
2019-10-11 10:37:01 +0200

    to main

---
+++ README.md
@@ -0,0 +1,1 @@
+# VueCoreBaseApp
---
+++ VueCoreApp/client-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <h1>Welcome to Your Vue.js App</h1>
+        <HelloWorld quote="this is the quote" author="roel"></HelloWorld>
+    </div>
+</template>
+
+<script lang="ts">
+    
+    import HelloWorld from '@/components/HelloWorld.vue';
+    import { Component, Vue } from 'vue-property-decorator';
+    
+    export default {
+        name:"app",
+        components: { HelloWorld },
+    };
+    
+</script>
